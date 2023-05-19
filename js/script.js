05b3c1919509a998792fc68ab1149a9896dc8efc
Nagendra Dubey <nagendrathegreat1998@gmail.com>
2023-05-19 18:02:34 +0530

    fourth commit

--- js/script.js
+++ js/script.js
@@ -27,25 +27,9 @@ function openPage(evt,pageName) {
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("tab1").click();
   
-  // sticky header
-  window.onscroll = function() {myFunction()};
-
-// Get the header
-var header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+  
 // Scroll to top button javascript code
-let mybutton = document.getElementById("myBtn");
+// let mybutton = document.getElementById("myBtn");
 
 // // When the user scrolls down 20px from the top of the document, show the button
 // window.onscroll = function() {scrollFunction()};
